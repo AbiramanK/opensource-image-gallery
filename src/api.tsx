9e52592be780efe25f05f3350d2d22a41f530c50
Abiraman K <abiramancit@gmail.com>
2023-03-02 17:08:55 +0530

    [Abiraman K] Integrates real apis

--- src/api.tsx
+++ src/api.tsx
@@ -1,3 +1,4 @@
+import { API, CLIENT_ID, RESULT_PER_PAGE } from "./constants";
 import { ImageByIdResult, images, imageSearchResult } from "./data";
 import {
   ImageByIdResultInterface,
@@ -5,29 +6,82 @@ import {
   SearchImageResultInterface,
 } from "./types";
 
-async function fetchPhotosList(): Promise<ImageInterface[]> {
+async function makeRequest(
+  url: string,
+  method: "GET" | "POST" | "PUT" | "UPDATE" | "DELETE"
+) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(images);
-    }, 3000);
+    return fetch(`${url}`, {
+      method: "get",
+    })
+      .then((response) => {
+        if (response?.status === 200 || response?.statusText === "OK") {
+          return response.json();
+        }
+
+        return reject();
+      })
+      .then((res) => {
+        return resolve(res);
+      })
+      .catch(() => {
+        reject();
+      });
   });
 }
 
+async function fetchPhotosList(): Promise<ImageInterface[]> {
+  return new Promise((resolve, reject) => {
+    return makeRequest(
+      `${API}/photos?client_id=${CLIENT_ID}&page=1&per_page=${RESULT_PER_PAGE}&order_by=latest`,
+      "GET"
+    ).then((response) => resolve(response as ImageInterface[]));
+  });
+}
+
+let controller: AbortController | undefined;
 async function fetchPhotosByQuery(
   query: string
 ): Promise<SearchImageResultInterface> {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(imageSearchResult);
-    }, 3000);
+    if (typeof controller != undefined) {
+      controller?.abort!();
+    }
+
+    controller = new AbortController();
+
+    return fetch(
+      `${API}/search/photos?client_id=${CLIENT_ID}&query=${query}&page=1&per_page=${RESULT_PER_PAGE}`,
+      {
+        method: "get",
+        signal: controller.signal,
+      }
+    )
+      .then((response) => {
+        if (response?.status === 200 || response?.statusText === "OK") {
+          return response.json();
+        }
+
+        return reject();
+      })
+      .then((res) => {
+        return resolve(res);
+      })
+      .catch((error) => {
+        if (error?.name === "AbortError") {
+          return resolve({} as SearchImageResultInterface);
+        }
+        return reject();
+      });
   });
 }
 
 async function fetchPhotoById(id: string): Promise<ImageByIdResultInterface> {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(ImageByIdResult);
-    }, 3000);
+    return makeRequest(
+      `${API}/photos/${id}?client_id=${CLIENT_ID}`,
+      "GET"
+    ).then((response) => resolve(response as ImageByIdResultInterface));
   });
 }
 
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
-const APP_DISPLAY_NAME = "Image Gallery";
+const APP_DISPLAY_NAME = process.env.REACT_APP_DISPLAY_NAME ?? "Image Gallery";
 
-export { APP_DISPLAY_NAME };
+const API = process.env.REACT_APP_API ?? "https://api.unsplash.com";
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? "";
+const RESULT_PER_PAGE = process.env.REACT_APP_RESULT_PER_PAGE ?? 12;
+
+export { APP_DISPLAY_NAME, API, CLIENT_ID, RESULT_PER_PAGE };
