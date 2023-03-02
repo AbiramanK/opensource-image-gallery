6eefbc8c7de45547a1a24db6d162cd4ea6120595
Abiraman K <abiramancit@gmail.com>
2023-03-02 09:44:16 +0530

    [Abiraman K] Refactors ImagePopup component

--- src/components/ImagePopup.tsx
+++ src/components/ImagePopup.tsx
@@ -102,198 +102,205 @@ function ImagePopup(props: ImagePopupProps) {
               justifyContent={"center"}
               sx={{ backgroundColor: "transparent" }}
             >
-              <Grid item xs={12} sm={6}>
+              <Grid item sx={{ display: "flex", justifyContent: "flex-end" }}>
                 <Box
                   component={"img"}
                   src={props?.image}
                   alt={props?.title}
                   width="100%"
+                  height={{ xs: "100%", sm: "85vh" }}
                 />
               </Grid>
               <Grid
                 item
                 xs={12}
                 sm={5}
-                sx={{ backgroundColor: colors.grey[100] }}
+                sx={{
+                  backgroundColor: colors.grey[100],
+                }}
               >
-                <Box sx={{ pl: 2, pt: 2 }}>
-                  <Grid container rowGap={3}>
-                    <Grid item xs={12}>
-                      <Grid container alignItems={"center"}>
-                        <Grid item xs={3} sm={2}>
-                          <Avatar
-                            sx={{ width: 56, height: 56 }}
-                            src={props?.authorPicture}
-                            alt={props?.authorName}
-                          />
-                        </Grid>
-                        <Grid item xs={9} sm={10}>
-                          <Grid container alignItems={"center"}>
-                            <Grid item xs={12}>
-                              <Typography
-                                variant="body1"
-                                color={colors.common.black}
-                                fontFamily={"Montserrat"}
-                                fontSize={16}
-                              >
-                                {props?.authorName}
-                              </Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                              <Typography
-                                variant="body2"
-                                color={colors.grey[600]}
-                                fontFamily={"Montserrat"}
-                                sx={{ fontStyle: "italic" }}
-                              >
-                                @{props?.authorUsername}
-                              </Typography>
-                            </Grid>
+                <Grid container sx={{ height: "100%" }}>
+                  <Grid item>
+                    <Grid container rowGap={3} sx={{ pl: 2, pt: 2 }}>
+                      <Grid item xs={12}>
+                        <Grid container alignItems={"center"}>
+                          <Grid item xs={3} sm={2}>
+                            <Avatar
+                              sx={{ width: 56, height: 56 }}
+                              src={props?.authorPicture}
+                              alt={props?.authorName}
+                            />
                           </Grid>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <Grid container>
-                        <Grid item>
-                          <Typography
-                            sx={{
-                              fontFamily: "Montserrat",
-                              color: colors.common.black,
-                            }}
-                          >
-                            Related Tags
-                          </Typography>
-                        </Grid>
-                        <Grid item>
-                          <Box
-                            id="related-tags-box"
-                            sx={{
-                              display: "flex",
-                              flexWrap: "wrap",
-                              maxHeight: 150,
-                              rowGap: 1,
-                              columnGap: 1,
-                              overflowY: "scroll",
-                              my: 2,
-                            }}
-                          >
-                            {props?.relatedTags?.map(
-                              (tag: string, index: number) => (
-                                <Button
-                                  key={index}
-                                  variant="outlined"
-                                  sx={{
-                                    textTransform: "none",
-                                    fontFamily: "Montserrat",
-                                    fontSize: 10,
-                                  }}
+                          <Grid item xs={9} sm={10}>
+                            <Grid container alignItems={"center"}>
+                              <Grid item xs={12}>
+                                <Typography
+                                  variant="body1"
+                                  color={colors.common.black}
+                                  fontFamily={"Montserrat"}
+                                  fontSize={16}
                                 >
-                                  {tag}
-                                </Button>
-                              )
-                            )}
-                          </Box>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Box>
-                <Box>
-                  <Grid
-                    container
-                    sx={{ pl: 2, pr: 4 }}
-                    alignItems={"center"}
-                    rowGap={1}
-                  >
-                    <Grid item xs={6} sm={8}>
-                      <Grid container>
-                        <Grid item>
-                          <IconButton sx={{ color: colors.grey[600] }}>
-                            <Share fontSize="large" />
-                          </IconButton>
-                        </Grid>
-                        <Grid item>
-                          <IconButton sx={{ color: colors.grey[600] }}>
-                            <Info fontSize="large" />
-                          </IconButton>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                    <Grid
-                      item
-                      xs={6}
-                      sm={4}
-                      sx={{ display: "flex", justifyContent: "flex-end" }}
-                    >
-                      <Button
-                        variant="outlined"
-                        color="success"
-                        startIcon={<Download />}
-                      >
-                        Download
-                      </Button>
-                    </Grid>
-                    <Grid item xs={12} sx={{ pb: 2 }}>
-                      <Grid container>
-                        <Grid
-                          item
-                          xs={6}
-                          sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                          }}
-                        >
-                          <Grid container alignItems={"center"}>
-                            <Grid item>
-                              <IconButton sx={{ color: colors.grey[600] }}>
-                                <Download fontSize="large" />
-                              </IconButton>
-                            </Grid>
-                            <Grid item>
-                              <Typography
-                                sx={{
-                                  fontFamily: "Montserrat",
-                                  color: colors.grey[800],
-                                }}
-                              >
-                                {props?.totalDownloads}
-                              </Typography>
+                                  {props?.authorName}
+                                </Typography>
+                              </Grid>
+                              <Grid item xs={12}>
+                                <Typography
+                                  variant="body2"
+                                  color={colors.grey[600]}
+                                  fontFamily={"Montserrat"}
+                                  sx={{ fontStyle: "italic" }}
+                                >
+                                  @{props?.authorUsername}
+                                </Typography>
+                              </Grid>
                             </Grid>
                           </Grid>
                         </Grid>
-                        <Grid item xs={6}>
-                          <Grid container justifyContent={"flex-end"}>
-                            <Grid item xs={4} sm={2}>
-                              <IconButton sx={{ color: colors.grey[600] }}>
-                                <ThumbUpAltOutlined fontSize="large" />
-                              </IconButton>
-                            </Grid>
-                            <Grid
-                              item
-                              xs={3}
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Grid container>
+                          <Grid item>
+                            <Typography
                               sx={{
-                                display: "flex",
-                                justifyContent: "center",
-                                alignItems: "center",
+                                fontFamily: "Montserrat",
+                                color: colors.common.black,
                               }}
                             >
-                              <Typography
+                              Related Tags
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Box
+                              id="related-tags-box"
+                              sx={{
+                                display: "flex",
+                                flexWrap: "wrap",
+                                maxHeight: 175,
+                                rowGap: 1,
+                                columnGap: 1,
+                                overflowY: "auto",
+                                my: 2,
+                              }}
+                            >
+                              {props?.relatedTags?.map(
+                                (tag: string, index: number) => (
+                                  <Button
+                                    key={index}
+                                    variant="outlined"
+                                    sx={{
+                                      textTransform: "none",
+                                      fontFamily: "Montserrat",
+                                      fontSize: 10,
+                                    }}
+                                  >
+                                    {tag}
+                                  </Button>
+                                )
+                              )}
+                            </Box>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={12}
+                    sx={{
+                      display: "flex",
+                      alignItems: "flex-end",
+                    }}
+                  >
+                    <Grid container sx={{ px: 1 }} alignItems={"center"}>
+                      <Grid item xs={6} sm={8}>
+                        <Grid container>
+                          <Grid item>
+                            <IconButton sx={{ color: colors.grey[600] }}>
+                              <Share fontSize="large" />
+                            </IconButton>
+                          </Grid>
+                          <Grid item>
+                            <IconButton sx={{ color: colors.grey[600] }}>
+                              <Info fontSize="large" />
+                            </IconButton>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                      <Grid
+                        item
+                        xs={6}
+                        sm={4}
+                        sx={{ display: "flex", justifyContent: "flex-end" }}
+                      >
+                        <Button
+                          variant="outlined"
+                          color="success"
+                          startIcon={<Download />}
+                        >
+                          Download
+                        </Button>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Grid container>
+                          <Grid
+                            item
+                            xs={6}
+                            sx={{
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center",
+                            }}
+                          >
+                            <Grid container alignItems={"center"}>
+                              <Grid item>
+                                <IconButton sx={{ color: colors.grey[600] }}>
+                                  <Download fontSize="large" />
+                                </IconButton>
+                              </Grid>
+                              <Grid item>
+                                <Typography
+                                  sx={{
+                                    fontFamily: "Montserrat",
+                                    color: colors.grey[800],
+                                  }}
+                                >
+                                  {props?.totalDownloads}
+                                </Typography>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                          <Grid item xs={6}>
+                            <Grid container justifyContent={"flex-end"}>
+                              <Grid item xs={4} sm={2}>
+                                <IconButton sx={{ color: colors.grey[600] }}>
+                                  <ThumbUpAltOutlined fontSize="large" />
+                                </IconButton>
+                              </Grid>
+                              <Grid
+                                item
+                                xs={3}
                                 sx={{
-                                  fontFamily: "Montserrat",
-                                  color: colors.grey[800],
+                                  display: "flex",
+                                  justifyContent: "center",
+                                  alignItems: "center",
                                 }}
                               >
-                                {props?.totalLikes}
-                              </Typography>
+                                <Typography
+                                  sx={{
+                                    fontFamily: "Montserrat",
+                                    color: colors.grey[800],
+                                  }}
+                                >
+                                  {props?.totalLikes}
+                                </Typography>
+                              </Grid>
                             </Grid>
                           </Grid>
                         </Grid>
                       </Grid>
                     </Grid>
                   </Grid>
-                </Box>
+                </Grid>
               </Grid>
             </Grid>
           </Box>
